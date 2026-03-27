3f74374dfe3be8b031d59946b9c39fa3a9cd142f
Sele-lab <ee14@kent.ac.uk>
2026-03-27 00:29:05 +0000

    assignment2

--- Contactlogbook.py
+++ Contactlogbook.py
@@ -3,3 +3,12 @@ class Contact:
         self.name = name
         self.phone = phone
         self.email = email
+
+    def display(self):
+        return f"{self.name} | {self.phone} | {self.email}"
+
+
+class FavouriteContact(Contact):
+    def __init__(self, name, phone, email):
+        super().__init__(name, phone, email)
+        self.favourite = True
